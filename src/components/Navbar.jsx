9ece9e34f6ec8bb7e7f1727648b0212fc5dc0794
Tomasz Smolarczyk <smolarczyk.thomas@gmail.com>
2023-05-23 12:29:33 +0200

    init

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import { close, logo, menu } from "../assets";
+import { navLinks } from "../constants";
+
+const Navbar = () => {
+  return (
+    <nav className="w-full flex py-6 justify-between items-center navbar">
+      <img src={logo} alt="logo-bank" className="w-[124px] h-[32px]" />
+      <ul className="list-none sm:flex hidden justify-end items-center flex-1 "></ul>
+    </nav>
+  );
+};
+
+export default Navbar;
